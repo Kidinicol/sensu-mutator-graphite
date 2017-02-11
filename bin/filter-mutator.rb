301d4acd8921b974bebb3c6b831baad4a6f18f4c
nikoletta.kyriakidou <nk8smg@hotmail.com>
2017-02-11 15:34:25 +0000

    fix offences

--- bin/filter-mutator.rb
+++ bin/filter-mutator.rb
@@ -18,46 +18,43 @@ require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'json'
 require 'sensu-mutator'
 class MutatorGraphite < Sensu::Mutator
-
 option :listtoinclude,
-    :description => "list of metrics to include",
-    :short => "-lti LISTTOINCLUDE",
-    :long => "--listtoinclude LISTTOINCLUDE",
-    :required => true
+       description: "list of metrics to include",
+       short: "-lti LISTTOINCLUDE",
+       long: "--listtoinclude LISTTOINCLUDE",
+       required: true
 
-  def mutate
-    metrics_to_include=Array.new
+ def mutate
+    metrics_to_include = []
     listfile = File.read(config[:listtoinclude])
-    list = JSON.parse(listfile, :symbolize_names => true)
+    list = JSON.parse(listfile, symbolize_names: true)
 
     metrics_to_include = list[:metrics_to_include]
 
     all_metrics = @event['check']['output'].split("\n")
 
-    cpu_metrics=Array.new
+    cpu_metrics = []
     all_metrics.each do |metric|
-      if (should_metric_be_included(metrics_to_include, metric))
+      if should_metric_be_included(metrics_to_include, metric)
         cpu_metrics.push(metric)
       end
     end
 
     # Add a key/value pair to indicate that the event data has been mutated.
-    @event.merge!(:mutated => true)
+    @event[:mutated] = true
     @event['check']['output'] = cpu_metrics.join("\n")
+ end
 
-  end
-
-  def dump
+ def dump
     puts @event['check']['output']
-  end
+ end
 
-  def should_metric_be_included(metrics_to_include, metric)
+ def should_metric_be_included(metrics_to_include, metric)
     metrics_to_include.each do |metric_to_include|
-	    if (metric.match(metric_to_include))
-	      return true
-		  end
+      if metric.match(metric_to_include)
+       return true
+      end
     end
     return false
-  end
-
+ end
 end
